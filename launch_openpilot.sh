5926303dd36494f27fb0ec5d3858ff6cb2f9fd0e
herizon1054 <spivbaby@gmail.com>
2026-09-09 01:33:08 +0800

    Update launch_openpilot.sh

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -2,5 +2,5 @@
 echo -n "2" > /data/params/d/HasAcceptedTerms
 echo -n "0.2.0" > /data/params/d/CompletedTrainingVersion
 echo -n "1" > /data/params/d/IsMetric
-export LITE=1
+
 exec ./launch_chffrplus.sh
